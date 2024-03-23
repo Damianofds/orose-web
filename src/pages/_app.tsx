fb47b376bf5537a522676309d34011a985ca40bd
Damianofds <damianofds@gmail.com>
2024-03-23 15:19:30 +0100

    Added analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,29 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+
 import { Parallax, ParallaxProvider } from 'react-scroll-parallax'
 
 export default function App({ Component, pageProps }: AppProps) {
+  
+  const router = useRouter();
+  
+  const isProd = process.env.NODE_ENV === "production";
+
+  useEffect(() => {
+    const handleRouteChange = (url: any) => {
+      if (isProd) {
+        window.gtag('config', process.env.NEXT_PUBLIC_GA_ID as string, {
+          page_path: url,
+        });
+      }
+    }
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    }
+  }, [router.events]);
+  
   return <ParallaxProvider><Component {...pageProps} /></ParallaxProvider>
-}
+}  
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,6 +1,11 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
+const gtag = `https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`;
+
 export default function Document() {
+
+  const isProd = process.env.NODE_ENV === "production";
+
   return (
     <Html lang='en'>
       <Head>
@@ -18,7 +23,25 @@ export default function Document() {
         <link href="https://fonts.googleapis.com/css2?family=Belanosima:wght@700&display=swap" rel="stylesheet"></link>
         <link href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:wght@300&display=swap" rel="stylesheet"></link>
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon.png" />
-      
+
+        {isProd && (
+        <>
+          <script async src={gtag} />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', {
+                  page_path: window.location.pathname
+                });
+              `,
+            }}
+          />
+          </>
+      )}
+
       </Head>
       <body>
         <Main />
@@ -27,3 +50,8 @@ export default function Document() {
     </Html>
   )
 }
+
+
+
+
+
